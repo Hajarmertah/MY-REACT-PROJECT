722672b83e54808c6b664328baab5e073342bb22
Hajarmertah <Hajar.mertah12@gmail.com>
2022-01-25 12:34:04 +0100

    init

--- src/pages/Private/PrivateHome/PrivateHome.js
+++ src/pages/Private/PrivateHome/PrivateHome.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import cat from "./cat.gif"
+import {Link} from 'react-router-dom'
 
 export default function PrivateHome() {
   return (
@@ -7,6 +8,7 @@ export default function PrivateHome() {
       <h1 className="display-3 text-light mb-4">
         LINK IT TO GRAPHES
       </h1>
+      <Link to={{pathname: "/BarChart"}} className='navbar-brand'>Entreprise X</Link>
       <img src={cat} />
     </div>
   )
