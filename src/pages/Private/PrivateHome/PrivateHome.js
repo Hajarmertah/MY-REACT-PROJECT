f7d561a5ea32dd5c1f6c2f9160bb9cf6b9f54d3e
Hajarmertah <Hajar.mertah12@gmail.com>
2022-01-25 13:39:36 +0100

    init

--- src/pages/Private/PrivateHome/PrivateHome.js
+++ src/pages/Private/PrivateHome/PrivateHome.js
@@ -6,10 +6,10 @@ export default function PrivateHome() {
   return (
     <div className="container p-5">
       <h1 className="display-3 text-light mb-4">
-        LINK IT TO GRAPHES
+        Les Entreprises
       </h1>
       <Link to={{pathname: "/BarChart"}} className='navbar-brand'>Entreprise X</Link>
-       <img src={cat} />
+       
     </div>
   )
 }
