ffc5518deaa9e957e18d6674f1eecbd77fcb8e5c
Hajarmertah <Hajar.mertah12@gmail.com>
2022-01-27 22:59:18 +0100

    init

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,7 @@ export default function Home() {
     return (
         
         <div className= "container p-5">
-           <h1 className="display-3 text-light">
+           <h1 className="h1">
                {currentUser ? "Welcome buddy" : "Hi, Sign Up or Sign In" }
             </h1>  
         </div>
