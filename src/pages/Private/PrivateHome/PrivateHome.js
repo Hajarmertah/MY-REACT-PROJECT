1954ec2d17607c91d125015eca991f2239864666
Hajarmertah <Hajar.mertah12@gmail.com>
2022-01-27 23:32:20 +0100

    init

--- src/pages/Private/PrivateHome/PrivateHome.js
+++ src/pages/Private/PrivateHome/PrivateHome.js
@@ -5,7 +5,7 @@ import {Link} from 'react-router-dom'
 export default function PrivateHome() {
   return (
     <div className="container p-5">
-      <h1 className="display-3 text-light mb-4">
+      <h1 className="h1">
         Les Entreprises
       </h1>
       <Link to={{pathname: "/BarChart"}} className='display-5 text-light mb-4'>Entreprise X</Link>
