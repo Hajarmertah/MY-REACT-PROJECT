74e9f98fab36f6db1d7bd01b4922314f88980c87
Hajarmertah <Hajar.mertah12@gmail.com>
2022-01-27 14:29:27 +0100

    init

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -1,29 +1,38 @@
+import { render } from "@testing-library/react";
 import React, { useState } from "react";
 import { Routes,Route, BrowserRouter } from "react-router-dom";
 import { BarChart, Bar, CartesianGrid, XAxis, YAxis } from 'recharts';
 import './comp.css';
 
 
-  
-const Barchart=() => {
-    const data = [
-        {mois:'Janvier', valeur: '155'},
-        {mois:'Février', valeur: '206'},
-        {mois:'Mars', valeur: '250.6'},
-        {mois:'Avril', valeur: '300'},
-        {mois:'Mai', valeur: '314.6'},
-        {mois:'Juin', valeur: '299.2'},
-        {mois:'Juillet', valeur: '265.3'},
-        {mois:'Aout', valeur: '322'},
-        {mois:'Septembre', valeur: '389.5'},
-        {mois:'Octobre', valeur: '265'},
-        {mois:'Novembre', valeur: '234'},
-        {mois:'Décembre', valeur: '306.2'}
+class Barchart extends React.Component{
+    state={
+        post: {}
+    }
 
-      ];
+    componentDidMount(){
+        fetch('/posts')
+        .then((res)=>{
+            return res.json();
+        })
+        .then((result)=>{
+            var x={post: result}
+            console.log(result)
+            var s=this.setState(x)
+            console.log(s)
+        })
+    }
+    
+render(){
+
+    const data=[
+        
+    ]
+
     return( 
         <>
              <h1 className="h1">Diagramme représantant la valeur de l'entreprise X cette année</h1>
+               
             <div className="graph">
                 <BarChart width={1000} height={600} data={data}>
                     <Bar dataKey="valeur" fill="green" />
@@ -32,8 +41,9 @@ const Barchart=() => {
                     <YAxis />
                 </BarChart>
             </div>
+            <div>  </div>
         </>
     )
 }
-
+}
 export default Barchart
